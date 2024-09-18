e62d5fb79f1e11f433ada5583b61f8d330da44da
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-09-18 12:28:48 +0200

    GardenShop

---
+++ frontend/src/components/GardenShop/GardenShop.tsx
@@ -0,0 +1,10 @@
+const GardenShop = () => {
+    return (
+      <>
+        <h2 className="font-bold text-blue-400">Din personliga handelsträdgård!</h2>
+      </>
+    );
+  };
+  
+  export default GardenShop;
+  
