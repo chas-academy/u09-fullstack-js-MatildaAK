fe48c1967ead207e2fd0197fafc587673f831a7f
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-09-20 12:19:47 +0200

    button component and combined in card

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -6,6 +6,7 @@ interface CardProps {
   backgroundSize?: "cover" | "contain";
   backgroundPosition?: "center" | "top" | "bottom";
   children: React.ReactNode;
+  footer?: React.ReactNode;
 }
 
 const Card: React.FC<CardProps> = ({
@@ -14,6 +15,7 @@ const Card: React.FC<CardProps> = ({
   backgroundSize = "cover",
   backgroundPosition = "center",
   children,
+  footer,
 }) => {
   return (
     <div
@@ -24,10 +26,13 @@ const Card: React.FC<CardProps> = ({
         backgroundPosition: backgroundPosition,
       }}
     >
-      <div className="px-8 pt-8 pb-16">
+      <div className="px-8 pt-8 pb-6">
         <div className="font-sans bg-primaryLightGreen dark:bg-primaryDarkGreen/50 backdrop-contrast-50 text-black dark:text-white rounded-lg max-w-lg mx-auto text-center">
           <h2 className="text-2xl p-3">{title}</h2>
           <p className="text-sm px-6 pt-4 pb-10 font-light">{children}</p>
+        </div>
+        <div className="pt-8 text-center">
+          {footer} 
         </div>
       </div>
     </div>
--- frontend/src/components/CoffeeInfo/CoffeeInfo.tsx
+++ frontend/src/components/CoffeeInfo/CoffeeInfo.tsx
@@ -1,12 +1,29 @@
 import Card from "../Card/Card";
 import coffeeImage from "../../assets/images/coffee.svg";
+import Button from "../Button/Button";
+import { useNavigate } from "react-router-dom";
 
 const CoffeeInfo = () => {
+  const navigate = useNavigate();
+
+  const goToCafe = () => {
+    navigate("/cafe");
+  };
     return (
       <>
       <Card 
         title="Njut av en fika i vårat café!"
-        backgroundImage={coffeeImage}>
+        backgroundImage={coffeeImage}
+        footer={(
+          <Button
+          type="button"
+          size="small"
+          variant="primary"
+          onClick={goToCafe}
+        >
+          Till cafét
+        </Button>
+        )}>
         Kom in på en härlig fikastund med hembakade kakor och lyxiga mackor.
       </Card>
       </>
