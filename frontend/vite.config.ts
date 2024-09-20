18fab70179deeda86b73c325b01c9b2f5c56bd09
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-09-20 10:49:14 +0200

    manifest file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,60 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './public/manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
     VitePWA({ 
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg', "/assets/images/book.svg", "/assets/images/coffee.svg", "/assets/images/snowdrops.svg"],
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,png,jpg,woff2,svg}'],  // Cachar fler filtyper
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/yourcdn\.com\/.*\.(png|jpg|jpeg|svg)$/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'images',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 30 * 24 * 60 * 60,
-              },
-            },
-          },
-        ],
-      },
-      manifest: {
-        name: 'MJs FlowerPot - Din handelsträdgård, bokhandel och café.',
-        short_name: 'MJs FlowerPot',
-        start_url: ".",
-        display: "standalone",
-        theme_color: '#ffffff',
-        icons: [
-            {
-                src: 'pwa-64x64.png',
-                sizes: '64x64',
-                type: 'image/png'
-            },
-            {
-                src: 'pwa-192x192.png',
-                sizes: '192x192',
-                type: 'image/png'
-            },
-            {
-                src: 'pwa-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'any'
-            },
-            {
-                src: 'maskable-icon-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'maskable'
-            }
-        ],
-      },
+      manifest: manifest,
+      workbox:{ 
+        globPatterns: ['**/*.{js,css,html,png,jpg,svg,ico}']
+      }
     })
   ],
 })
