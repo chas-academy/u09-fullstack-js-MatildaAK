6413c86dd1fd5a76f6ff39d561e3c770dc72c5b5
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-09-23 09:19:27 +0200

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,17 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <>
-      <div>
+      {/* <div>
         <Header />
 
         <main>
           <Outlet />
         </main>
-      </div>
+      </div> */}
+
+      <Header />
+
+      <Outlet />
     </>
   );
 }
