19bfe0fc63a7cd894259fcf1001605352acaa009
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-09-24 11:14:47 +0200

    popular item

---
+++ frontend/src/assets/popular.ts
@@ -0,0 +1,37 @@
+import p1_img from "../assets/images/itEndsWithUs.png";
+import p2_img from "../assets/images/itStartWithUs.png";
+import p3_img from "../assets/images/november9.png";
+import p4_img from "../assets/images/uglyLove.png";
+
+const POPULAR = [
+    {
+        id:1,
+        title: "It Ends With Us",
+        author: "Colleen Hoover",
+        image:p1_img,
+        price: 149,
+    },
+    {
+        id:2,
+        title: "It Start With Us",
+        author: "Colleen Hoover",
+        image:p2_img,
+        price: 149,
+    },
+    {
+        id:3,
+        title: "November 9",
+        author: "Colleen Hoover",
+        image:p3_img,
+        price: 149,
+    },
+    {
+        id:4,
+        title: "Ugly Love",
+        author: "Colleen Hoover",
+        image:p4_img,
+        price: 149,
+    },
+]
+
+export default POPULAR;
