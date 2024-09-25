33ab0075837bb2e7e5ce51decc703d6b9bb85480
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-09-25 16:43:54 +0200

    styling change

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { faSquareFacebook, faInstagram } from '@fortawesome/free-brands-svg-icon
 
 const Footer = () => {
     return (
-        <footer className='bg-primaryLightGreen dark:bg-primaryDarkGreen'>
+        <footer className='relative inset-0 bg-primaryLightGreen dark:bg-primaryDarkGreen'>
             <div className="text-black dark:text-white font-sans flex justify-center">
                 <div className="flex justify-between gap-x-10 py-4 md:gap-x-20">
                     <a href="#" className='hover:text-thirdDarkBlue dark:hover:text-thirdLightBlue'>FAQ</a>
