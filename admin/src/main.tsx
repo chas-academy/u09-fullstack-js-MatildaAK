734e46a19fe6450902b58781aef39161d81ca5a0
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-10-04 12:33:35 +0200

    added routes

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App.tsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Admin from './pages/Admin/Admin.tsx';
+import AddProduct from './components/Products/AddProduct/AddProduct.tsx';
+import ListProduct from './components/Products/ListProduct/ListProduct.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <App />
+      </>
+    ),
+    children: [
+      {
+        path: "",
+        element: <Admin />,
+      },
+      {
+        path: "/skapa",
+        element: <AddProduct />
+      },
+      {
+        path: "/produkter",
+        element: <ListProduct />
+      }
+
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
