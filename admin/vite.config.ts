52c735e3b926161b0f7e0f0c8d15293aee6abfda
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-10-21 18:56:59 +0200

    pwa

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './public/manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest: manifest,
+      workbox:{ 
+        globPatterns: ['**/*.{js,css,html,png,jpg,svg,ico}']
+      }
+    })
+  ],
 })
